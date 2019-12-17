c4aa2d36654cdd6c3989737ba9974b156eac96c2
Aeson Johnson <aeson.m.johnson@gmail.com>
2019-12-17 13:35:15 -0800

    aeson-johnson
    the links stopped working for some reason?? fixed it though

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Navigation />
+    <BrowserRouter>
+      <Navigation />
+      <Route exact path = '/Home'><Home /></Route>
+      <Route path = '/About'><About /></Route>
+      <Route path = '/Contact'><Contact /></Route>
+    </BrowserRouter>
   </div>
 );
 
